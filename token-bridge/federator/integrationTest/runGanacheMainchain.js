8137b4fc3f1bba9e9cd8d0108c66731eff9daf4a
rajaroy43 <rajaroy674@gmail.com>
2021-06-16 01:08:10 +0530

    Integeration test updated using federator off-chain

--- token-bridge/federator/integrationTest/runGanacheMainchain.js
+++ token-bridge/federator/integrationTest/runGanacheMainchain.js
@@ -1,15 +1,14 @@
-const { execSync } = require('child_process');
-const { networks } = require('../../bridge/truffle-config')
+const { execSync } = require("child_process");
+const { networks } = require("../../bridge/truffle-config");
 
-const rskConfig = networks.rskregtest
+const rskConfig = networks.development2;
 execSync(
-    `npx ganache-cli \
+  `npx ganache-cli \
         -p ${rskConfig.port} \
         -i ${rskConfig.network_id} \
         --chainId ${rskConfig.network_id} \
         -g ${rskConfig.gasPrice} \
         -l ${rskConfig.gas} \
         -b 0.1`,
-    { stdio: 'inherit' }
+  { stdio: "inherit" }
 );
-
