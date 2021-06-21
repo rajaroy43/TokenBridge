6542db86475b8a0420610ff2faa36b4bb721cd7d
rajaroy43 <rajaroy674@gmail.com>
2021-06-21 23:29:17 +0530

    Confirmation added for public testnet rinkeby and binance

--- token-bridge/federator/config/config.js
+++ token-bridge/federator/config/config.js
@@ -7,8 +7,8 @@ try {
   telegramToken = "";
 }
 module.exports = {
-  mainchain: require("./development.json"), //the json containing the smart contract addresses in rsk
-  sidechain: require("./development2.json"), //the json containing the smart contract addresses in eth
+  mainchain: require("./rinkeby.json"), //the json containing the smart contract addresses in rsk
+  sidechain: require("./btestnet.json"), //the json containing the smart contract addresses in eth
   runEvery: 2, // In minutes,
   //confirmations: 0, // Number of blocks before processing it, if working with ganache set as 0
   //now we have confirmation table ,so we don't use confirmations
@@ -23,6 +23,154 @@ module.exports = {
     1: {
       default: 5760,
       minConfirmation: 10,
+      WETH: [
+        {
+          amount: 0,
+          confirmations: 10,
+        },
+        {
+          amount: 0.2,
+          confirmations: 30,
+        },
+        {
+          amount: 0.5,
+          confirmations: 50,
+        },
+      ],
+      WBTC: [
+        {
+          amount: 0,
+          confirmations: 10,
+        },
+        {
+          amount: 0.2,
+          confirmations: 30,
+        },
+        {
+          amount: 0.5,
+          confirmations: 50,
+        },
+      ],
+      DAI: [
+        {
+          amount: 0,
+          confirmations: 10,
+        },
+        {
+          amount: 0.2,
+          confirmations: 30,
+        },
+        {
+          amount: 0.5,
+          confirmations: 50,
+        },
+      ],
+      renBTC: [
+        {
+          amount: 0,
+          confirmations: 10,
+        },
+        {
+          amount: 0.2,
+          confirmations: 30,
+        },
+        {
+          amount: 0.5,
+          confirmations: 50,
+        },
+      ],
+    },
+    4: {
+      default: 12,
+      minConfirmation: 10,
+      WETH: [
+        {
+          amount: 0,
+          confirmations: 10,
+        },
+        {
+          amount: 0.2,
+          confirmations: 11,
+        },
+        {
+          amount: 0.5,
+          confirmations: 12,
+        },
+      ],
+      MAIN: [
+        {
+          amount: 0,
+          confirmations: 10,
+        },
+        {
+          amount: 0.2,
+          confirmations: 30,
+        },
+        {
+          amount: 0.5,
+          confirmations: 50,
+        },
+      ],
+      WBTC: [
+        {
+          amount: 0,
+          confirmations: 10,
+        },
+        {
+          amount: 0.2,
+          confirmations: 30,
+        },
+        {
+          amount: 0.5,
+          confirmations: 50,
+        },
+      ],
+      DAI: [
+        {
+          amount: 0,
+          confirmations: 10,
+        },
+        {
+          amount: 0.2,
+          confirmations: 30,
+        },
+        {
+          amount: 0.5,
+          confirmations: 50,
+        },
+      ],
+      renBTC: [
+        {
+          amount: 0,
+          confirmations: 10,
+        },
+        {
+          amount: 0.2,
+          confirmations: 30,
+        },
+        {
+          amount: 0.5,
+          confirmations: 50,
+        },
+      ],
+    },
+    97: {
+      default: 12,
+      minConfirmation: 10,
+      MAIN: [
+        {
+          amount: 0,
+          confirmations: 10,
+        },
+        {
+          amount: 0.2,
+          confirmations: 11,
+        },
+        {
+          amount: 0.5,
+          confirmations: 12,
+        },
+      ],
       WETH: [
         {
           amount: 0,
