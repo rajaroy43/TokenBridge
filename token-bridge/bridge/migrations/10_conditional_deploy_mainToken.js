1f4a51b3aead79cbeb7c0e177217f3569c6fb974
rajaroy43 <rajaroy674@gmail.com>
2021-06-21 23:19:24 +0530

    Not deploy Fake Token on binance testnet

--- token-bridge/bridge/migrations/10_conditional_deploy_mainToken.js
+++ token-bridge/bridge/migrations/10_conditional_deploy_mainToken.js
@@ -1,17 +1,18 @@
-
-const MainToken = artifacts.require('MainToken');
+const MainToken = artifacts.require("MainToken");
 
 function shouldDeployToken(network) {
-    return !network.toLowerCase().includes('mainnet') &&
-    !network.toLowerCase().includes('kovan') &&
-    !network.toLowerCase().includes('testnet');
+  return (
+    !network.toLowerCase().includes("mainnet") &&
+    !network.toLowerCase().includes("kovan") &&
+    !network.toLowerCase().includes("btestnet") &&
+    !network.toLowerCase().includes("ropsten")
+  );
 }
 
 module.exports = function(deployer, networkName, accounts) {
-    deployer
-    .then(() => {
-        if(shouldDeployToken(networkName)) {
-            return deployer.deploy(MainToken, 'MAIN', 'MAIN', 18, web3.utils.toWei('1000'));
-        }
-    });
-}
+  deployer.then(() => {
+    if (shouldDeployToken(networkName)) {
+      return deployer.deploy(MainToken, "MAIN", "MAIN", 18, web3.utils.toWei("1000"));
+    }
+  });
+};
